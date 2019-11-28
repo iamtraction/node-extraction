07d4570502f8ee232ad8ffab8113c0997ebb1279
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-11-28 21:08:52 +0530

    add typings

---
+++ typings/main.d.ts
@@ -0,0 +1,9 @@
+﻿import * as Request from "request-promise-native";
+declare class ExtractionWrapper {
+    private BASE_URL;
+    private OPTIONS;
+    constructor(options?: Request.RequestPromiseOptions);
+    request(path: string, options?: Request.RequestPromiseOptions): Promise<any>;
+}
+export = ExtractionWrapper;
+//# sourceMappingURL=main.d.ts.map
